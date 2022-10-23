73444a6faa883d85c0f0bb2434380f3632ffae13
Xiangz4 <975775303@qq.com>
2022-10-23 21:39:12 +0800

    first

---
+++ src/main/java/com/xyf/reids_and_caffeine/ReidsAndCaffeineApplication.java
@@ -0,0 +1,13 @@
+package com.xyf.reids_and_caffeine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReidsAndCaffeineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReidsAndCaffeineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xyf/reids_and_caffeine/ReidsAndCaffeineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyf.reids_and_caffeine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReidsAndCaffeineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
